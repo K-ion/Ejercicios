57c5490f82fea06e5dee12b9a9072f2f88d0fd0a
K-ion <84050298+K-ion@users.noreply.github.com>
2022-08-23 20:52:56 -0300

    Add files via upload

---
+++ ejercicio 1/src/Main.java
@@ -0,0 +1,16 @@
+//Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+//Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+
+
+public class Main {
+    public static void main(String[] args) {
+      var  numeroif = 20;
+
+      if (numeroif <=-1) {
+            System.out.println("el valor es negativo");
+        }
+      else if (numeroif >=0) {
+            System.out.println("El valor es positivo");
+        }
+    }
+}
---
+++ ejercicio 3/src/Main.java
@@ -0,0 +1,17 @@
+//Para el bucle Do While, deberás crear la misma estructura que en el While,
+// pero solo se debe ejecutar una vez.
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        var numeroWhile = 0;
+
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        } while (numeroWhile < 3);
+
+
+    }
+}
